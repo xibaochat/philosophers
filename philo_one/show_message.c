154e5d8780a7f2da1de82bb60bb0d9f88ead4ba6
xibaochat <wmmxxbb@gmail.com>
2021-04-18 22:19:19 +0200

    printf_message

--- philo_one/show_message.c
+++ philo_one/show_message.c
@@ -18,12 +18,15 @@ void	printf_message(t_phi *p, char *s)
 {
 	unsigned long	t;
 
-	if (p->simu->has_death == 1 && p->simu->is_died != 1)
-        return ;
-    else if (p->simu->is_died)
-        p->simu->is_died = 0;
+	if (p->simu->has_death)
+	{
+		if (p->simu->is_died == 0)
+			return ;
+	}
 	pthread_mutex_lock(&p->simu->display);
 	t = p->simu->start_time;
 	printf("%ld %d %s\n", get_timestamp(t), p->phi_id + 1, s);
 	pthread_mutex_unlock(&p->simu->display);
+	if (p->simu->is_died)
+		p->simu->is_died = 0;
 }
